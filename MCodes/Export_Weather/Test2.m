42e19e3769a36571cbb6c1dcd55a2dc15203503c
Mohammad Abouali <maboualiedu@gmail.com>
2015-10-16 17:31:18 -0400

    Supporting Old and New Format
    Changes were made in export_IFSM_Weather.m to support both new format
    and old format. Old Format did not have mean temperature and wind speed.
    Also the program now make sure that there are only 365 days in a year.
    For leap year, the last record is dropped to make it down to 366 days.
    That corresponds to Dec 31. It was decided to do that to avoid one day
    shift within the growing season.

--- MCodes/Export_Weather/Test2.m
+++ MCodes/Export_Weather/Test2.m
@@ -13,7 +13,13 @@ Station.NitrogenLevel=0.5;
 Data=readtable('KBSData.csv');
 Data=table2struct(Data,'ToScalar',true);
 
+%% Getting Option structure with default values
+Options=export_IFSM_Weather();
+
+%% Making sure that old IFSM format is used
+Options.useOldFormat=true;
+
 %% Exporting to IFSM WeatherFileFormat
 tic;
-export_IFSM_Weather('IFSM_Weather_NewFormat.wth', Data,Station);
+export_IFSM_Weather('IFSM_Weather_OldFormat.wth', Data,Station,Options);
 toc;
--- MCodes/Export_Weather/export_IFSM_Weather.m
+++ MCodes/Export_Weather/export_IFSM_Weather.m
@@ -1,4 +1,20 @@
-function export_IFSM_Weather(filename, Data,Station)
+function [Options]=export_IFSM_Weather(filename, Data,Station,Options)
+%% Checking Options content
+if (nargin<4 || isempty(Options))
+  Options.useOldFormat=false;
+else
+  if (~isfield(Options,'useOldFormat') || ...
+      ~islogical(Options.useOldFormat) || ...
+      isempty(Options.useOldFormat))
+    Options.useOldFormat=false;
+  end
+end
+
+%% getting default Options
+if (nargin<1)
+  return
+end
+
 %% Checking inputs
 validateattributes(filename,{'char'},{'row'});
 
@@ -8,8 +24,15 @@ if (~isstruct(Data) || ~isstruct(Station))
 end
 
 % making sure all the fields exists
-if any((~isfield(Data,{'Year','Month','Day','SRad','Tmean','Tmax','Tmin','TotalPr','MeanWindSpeed'})))
-  error('Data must contain all the following fields (case sensitive): Year, Month, Day, SRad, Tmean, Tmax, Tmin, TotalPr, MeanWindSpeed.')
+switch Options.useOldFormat
+  case false
+    if any((~isfield(Data,{'Year','Month','Day','SRad','Tmean','Tmax','Tmin','TotalPr','MeanWindSpeed'})))
+      error('When using new format, Data must contain all the following fields (case sensitive): Year, Month, Day, SRad, Tmean, Tmax, Tmin, TotalPr, MeanWindSpeed.')
+    end
+  case true
+    if any((~isfield(Data,{'Year','Month','Day','SRad','Tmax','Tmin','TotalPr',})))
+      error('When using old format, Data must contain all the following fields (case sensitive): Year, Month, Day, SRad, Tmax, Tmin, TotalPr.')
+    end
 end
 
 if any((~isfield(Station,{'Name','Lat','Lon','CO2Level','NitrogenLevel'})))
@@ -22,11 +45,13 @@ nData=numel(Data.Year);
 validateattributes(Data.Month,{'numeric'},{'vector','numel',nData});
 validateattributes(Data.Day,{'numeric'},{'vector','numel',nData});
 validateattributes(Data.SRad,{'numeric'},{'vector','numel',nData});
-validateattributes(Data.Tmean,{'numeric'},{'vector','numel',nData});
 validateattributes(Data.Tmax,{'numeric'},{'vector','numel',nData});
 validateattributes(Data.Tmin,{'numeric'},{'vector','numel',nData});
 validateattributes(Data.TotalPr,{'numeric'},{'vector','numel',nData});
-validateattributes(Data.MeanWindSpeed,{'numeric'},{'vector','numel',nData});
+if (~Options.useOldFormat)
+  validateattributes(Data.Tmean,{'numeric'},{'vector','numel',nData});
+  validateattributes(Data.MeanWindSpeed,{'numeric'},{'vector','numel',nData});
+end
 
 %% Preparing date column
 % sorting data based on date if needed
@@ -41,11 +66,13 @@ if (any(dDateNumber<0))
   Data.Month=Data.Month(idx);
   Data.Day=Data.Day(idx);
   Data.SRad=Data.SRad(idx);
-  Data.Tmean=Data.Tmean(idx);
   Data.Tmax=Data.Tmax(idx);
   Data.Tmin=Data.Tmin(idx);
   Data.TotalPr=Data.TotalPr(idx);
-  Data.MeanWindSpeed=Data.MeanWindSpeed(idx);
+  if (~Options.useOldFormat)
+    Data.Tmean=Data.Tmean(idx);
+    Data.MeanWindSpeed=Data.MeanWindSpeed(idx);
+  end
 else
   if (any(dDateNumber==0))
     error('Repeated date in the data.')
@@ -56,6 +83,22 @@ end
 % now ready to calculate the date based on IFSM Format, i.e. YYDDD
 twoDigitYear=mod(Data.Year,100);
 DayOfYear=dateNumber-datenum(Data.Year,1,1)+1;
+% Removing last day of the leap years to make them 365 days
+% IFSM only accepts 365 days in a year.
+mask= (DayOfYear~=366);
+twoDigitYear=twoDigitYear(mask);
+DayOfYear=DayOfYear(mask);
+Data.Year=Data.Year(mask);
+Data.Month=Data.Month(mask);
+Data.Day=Data.Day(mask);
+Data.SRad=Data.SRad(mask);
+Data.Tmax=Data.Tmax(mask);
+Data.Tmin=Data.Tmin(mask);
+Data.TotalPr=Data.TotalPr(mask);
+if (~Options.useOldFormat)
+  Data.Tmean=Data.Tmean(mask);
+  Data.MeanWindSpeed=Data.MeanWindSpeed(mask);
+end
 YYDDD=twoDigitYear*1000+DayOfYear;
 
 %% Making sure data is in row vector
@@ -64,9 +107,6 @@ if (~isrow(YYDDD))
 end
 if (~isrow(Data.SRad))
   Data.SRad=reshape(Data.SRad,1,[]);
-end
-if (~isrow(Data.Tmean))
-  Data.Tmean=reshape(Data.Tmean,1,[]);
 end
 if (~isrow(Data.Tmax))
   Data.Tmax=reshape(Data.Tmax,1,[]);
@@ -77,8 +117,13 @@ end
 if (~isrow(Data.TotalPr))
   Data.TotalPr=reshape(Data.TotalPr,1,[]);
 end
-if (~isrow(Data.MeanWindSpeed))
-  Data.MeanWindSpeed=reshape(Data.MeanWindSpeed,1,[]);
+if (~Options.useOldFormat)
+  if (~isrow(Data.Tmean))
+    Data.Tmean=reshape(Data.Tmean,1,[]);
+  end
+  if (~isrow(Data.MeanWindSpeed))
+    Data.MeanWindSpeed=reshape(Data.MeanWindSpeed,1,[]);
+  end
 end
 
 %% Opening the output file
@@ -101,10 +146,15 @@ try
           Station.NitrogenLevel);
         
 	% Writing the data
-  fprintf(fid, ...
-          '%5g%6.1f%6.1f%6.1f%6.1f%6.1f%6.1f\r\n', ...
-          [YYDDD; Data.SRad; Data.Tmean; Data.Tmax; Data.Tmin; Data.TotalPr; Data.MeanWindSpeed]);
-        
+  if (Options.useOldFormat)
+    fprintf(fid, ...
+            '%5g%6.1f%6.1f%6.1f%6.1f\r\n', ...
+            [YYDDD; Data.SRad; Data.Tmax; Data.Tmin; Data.TotalPr;]);
+  else
+    fprintf(fid, ...
+            '%5g%6.1f%6.1f%6.1f%6.1f%6.1f%6.1f\r\n', ...
+            [YYDDD; Data.SRad; Data.Tmean; Data.Tmax; Data.Tmin; Data.TotalPr; Data.MeanWindSpeed]);
+  end
 catch ME
   fclose(fid);
   rethrow(ME);
